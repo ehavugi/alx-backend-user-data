fdae11d5e2d5262144fb9ef41296d0779b002834
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-01 02:46:56 +0000

    reduced number of lines for filter logger functions

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,16 +16,12 @@ conv = datetime.datetime.fromtimestamp
 PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'ip')
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
-    """return a redacted test without values for fields.
-    uses a separator to separate fields.
-    """
+def filter_datum(fields: List[str], reda: str, msg: str, sep: str) -> str:
+    """return a redacted test without values for fields."""
     for field in fields:
-        replacement = "{}={}{}".format(field, redaction, seperator)
-        message = re.sub(r'{}=[^{}]+{}'.format(field, seperator, seperator),
-                         replacement, message)
-    return message
+        rep = "{}={}{}".format(field, reda, sep)
+        msg = re.sub(r'{}=[^{}]+{}'.format(field, sep, sep), rep, msg)
+    return msg
 
 
 class RedactingFormatter(logging.Formatter):
@@ -46,15 +42,12 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """format a record record by redacting the fields and reformating the
-        the log
-        """
-        r = record
-        redactedmsg = filter_datum(self.fields, self.REDACTION,
-                                   str(record.msg), self.SEPARATOR)
-        ascii_time = conv(record.created).strftime('%Y-%m-%d %H:%M:%S,%f3')
+        """format a record record by redacting the fields and reformating"""
+        r, f = record, filter_datum
+        rm = f(self.fields, self.REDACTION, str(record.msg), self.SEPARATOR)
+        ascii_time = conv(r.created).strftime('%Y-%m-%d %H:%M:%S,%f3')
         return str(self.FORMAT % {"name": r.name, "levelname": r.levelname,
-                   "asctime": ascii_time, "message": redactedmsg})
+                   "asctime": ascii_time, "message": rm})
 
 
 def get_logger() -> logging.Logger:
@@ -77,3 +70,18 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     conn = connectori.MySQLConnection(user=user, database=db,
                                       host=host, password=password)
     return conn
+
+
+def main() -> None:
+    conn = get_db()
+    cur = conn.cursor(buffered=True)
+    cur.execute("SELECT * from users;")
+    print(cur.fetchone())
+    cur.close()
+    conn.close()
+    print("hello")
+    return None
+
+
+if __name__ == "__main__":
+    main()
