cabda8a8b9eb18394ed42dfb147df09e5b2c802f
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-04 06:30:07 +0000

    added auth template class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,12 +16,25 @@ class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """A authorization handler
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None:
+            return True
+        if len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        if str(path) + "/" in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authorization header reader
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """return a current user.
