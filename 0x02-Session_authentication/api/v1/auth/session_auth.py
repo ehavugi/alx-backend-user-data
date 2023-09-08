8c79d430e5bcce90c0f43b8f01034eb512fec5df
Emmanuel HAVUGIMANA <havugimanaemmanuel2@gmail.com>
2023-09-08 01:58:31 +0000

    added session id generation

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -5,6 +5,7 @@ from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
 from typing import TypeVar
+import uuid
 
 
 class SessionAuth(Auth):
@@ -12,3 +13,16 @@ class SessionAuth(Auth):
         authorization header getter
         authorization header decoder
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """create a session given user_id
+        """
+        if user_id is None:
+            return None
+        if not(isinstance(user_id, str)):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
